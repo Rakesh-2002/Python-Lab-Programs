35c908e038e1c0b7c9913b594cc5720ee31c9f10
Rakesh-2002 <75826932+Rakesh-2002@users.noreply.github.com>
2021-03-28 14:50:59 +0530

    Update 12_sort_numbers_text_file_lists.py

--- 12_sort_numbers_text_file_lists.py
+++ 12_sort_numbers_text_file_lists.py
@@ -1,1 +1,8 @@
 """ Design a Python script to sort numbers specified in a text file using lists."""
+file_obj = open("numbers.txt", "b")
+num_list = []
+for num in file_obj:
+    c = num.replace("\n", '')
+    num_list.append(int(c))
+num_list.sort()
+print(num_list)
