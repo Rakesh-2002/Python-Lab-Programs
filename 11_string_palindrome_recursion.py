c580c8538b9a329a98eda735078c4639123cf565
Rakesh-2002 <75826932+Rakesh-2002@users.noreply.github.com>
2021-03-29 10:04:20 +0530

    Update 11_string_palindrome_recursion.py

--- 11_string_palindrome_recursion.py
+++ 11_string_palindrome_recursion.py
@@ -1,1 +1,14 @@
 """ Design a Python script to determine if a given string is a Palindrome using recursion. """
+def is_palindrome(s):
+    if len(s) < 1:
+        return True
+    else:
+        if s[0] == s[-1]:
+            return is_palindrome(s[1:-1])
+        else:
+            return False
+a=str(input("Enter string:"))
+if(is_palindrome(a)==True):
+    print("String is a palindrome!")
+else:
+    print("String isn't a palindrome!")
